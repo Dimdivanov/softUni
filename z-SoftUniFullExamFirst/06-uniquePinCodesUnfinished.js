0f58a02a40eec0456725807dd59703bf16d7ede9
Dimitar Ivanov <riseabove92@yahoo.com>
2023-06-10 22:42:16 +0300

    main

--- z-SoftUniFullExamFirst/06-uniquePinCodesUnfinished.js
+++ z-SoftUniFullExamFirst/06-uniquePinCodesUnfinished.js
@@ -4,20 +4,9 @@ function uniquePinCodes(input) {
   let num3 = Number(input[2]);
 
   for (let i = 1; i <= num1; i++) {
-    // if (i % 2 === 0) {
-    //   result = `${i}`;
-    // }
     for (let j = 1; j <= num2; j++) {
-      // if (j % 2 === 0) {
-      //   result = `${j}`;
-      // }
       for (let k = 1; k <= num3; k++) {
-        let result = 0;
-
-        if (k % 2 === 0) {
-          result = `${k}`;
-        }
-        console.log(`${k}`);
+        console.log(`${i}`);
       }
     }
   }
