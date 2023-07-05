479a19f09730c77f7872d3d3ecc5f144e5bda14c
Dimitar Ivanov <riseabove92@yahoo.com>
2023-07-05 13:30:10 +0300

    main

--- MakingShapes/05-squareFrame.js
+++ MakingShapes/05-squareFrame.js
@@ -1,13 +1,15 @@
 function squareFrame(input) {
   let n = Number(input[0]);
-
-  for (let i = 1; i <= n; i++) {
-    let lineBuild = '';
-
-    for (let j = 1; j <= n; j++) {
-      lineBuild += '+ ';
+  let lineBuild = '';
+  for (let i = 1; i <= 1; i++) {
+    lineBuild += '+ ' + '- '.repeat(n - 2) + '+';
+    for (let k = 1; k <= n - 2; k++) {
+      lineBuild += '\n' + '| ' + '- '.repeat(n - 2) + '|';
+    }
+    for (let j = 1; j <= 1; j++) {
+      lineBuild += '\n' + '+ ' + '- '.repeat(n - 2) + '+';
     }
     console.log(lineBuild);
   }
 }
-squareFrame(['4']);
+squareFrame(['6']);
