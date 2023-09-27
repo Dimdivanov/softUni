40c322436d12142b4d947234cc64ccfc2e719c95
Dimitar Ivanov <riseabove92@yahoo.com>
2023-09-27 14:57:05 +0300

    main

--- JSFundamentals/dataTypesAndVariablesLab/09-specialNumbers.js
+++ JSFundamentals/dataTypesAndVariablesLab/09-specialNumbers.js
@@ -1,17 +1,18 @@
 function specialNumbers(n) {
   for (let i = 1; i <= n; i++) {
-
     let curNum = Number(i);
-    
-    for (let k = 0){
 
-    }
+    let firstDig = parseInt(curNum % 10);
+    let lastDigit = parseInt(curNum / 10);
+    let combo = firstDig + lastDigit;
 
     if (curNum == 5 || curNum == 7) {
+      console.log(`${curNum} -> True`);
+    } else if (combo === 5 || combo == 7 || combo == 11) {
       console.log(`${curNum} -> True`);
     } else {
       console.log(`${curNum} -> False`);
     }
   }
 }
-specialNumbers(15);
+specialNumbers(56);
