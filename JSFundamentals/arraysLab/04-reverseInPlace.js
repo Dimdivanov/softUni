3ae75195273b05343ca1e8ab1be73f262d0521db
Dimitar Ivanov <riseabove92@yahoo.com>
2023-09-29 17:08:00 +0300

    arry lab

--- JSFundamentals/arraysLab/04-reverseInPlace.js
+++ JSFundamentals/arraysLab/04-reverseInPlace.js
@@ -1,4 +1,11 @@
-function reverseInPlace(arry) {
-  console.log(arry.reverse().join(' '));
+function reverseInPlace(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let j = arr.length - 1 - i;
+
+    let bonus = arr[i];
+    arr[i] = arr[j];
+    arr[j] = bonus;
+  }
+  console.log(arr);
 }
 reverseInPlace(['a', 'b', 'c', 'd', 'e']);
