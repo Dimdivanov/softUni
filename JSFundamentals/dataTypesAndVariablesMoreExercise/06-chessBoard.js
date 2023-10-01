182327cda2978b6a9048f53308cb49d1be88de22
Dimitar Ivanov <riseabove92@yahoo.com>
2023-10-01 23:44:32 +0300

    reworked working fine

--- JSFundamentals/dataTypesAndVariablesMoreExercise/06-chessBoard.js
+++ JSFundamentals/dataTypesAndVariablesMoreExercise/06-chessBoard.js
@@ -1,13 +1,15 @@
 function chessBoard(n) {
   console.log('<div class="chessboard">');
+  let currentColor = '    <span class="black"></span>';
   for (let i = 0; i < n; i++) {
     console.log('  <div>');
-
-    for (let j = 1; j < n; j++) {
-      if (j % 2 == 0) {
+    for (let j = 0; j < n; j++) {
+      if (currentColor === '    <span class="black"></span>') {
         console.log('    <span class="black"></span>');
+        currentColor = '    <span class="white"></span>';
       } else {
         console.log('    <span class="white"></span>');
+        currentColor = '    <span class="black"></span>';
       }
     }
 
