13737681d3dfd5b0d71137b72048741d87b909aa
Dimitar Ivanov <riseabove92@yahoo.com>
2023-10-01 23:30:51 +0300

    reworked

--- JSFundamentals/dataTypesAndVariablesMoreExercise/06-chessBoard.js
+++ JSFundamentals/dataTypesAndVariablesMoreExercise/06-chessBoard.js
@@ -1,17 +1,14 @@
 function chessBoard(n) {
-  let position = 0;
-
   console.log('<div class="chessboard">');
   for (let i = 0; i < n; i++) {
     console.log('  <div>');
 
-    for (let j = 0; j < n; j++) {
-      if (position % 2 == 0) {
+    for (let j = 1; j < n; j++) {
+      if (j % 2 == 0) {
         console.log('    <span class="black"></span>');
       } else {
         console.log('    <span class="white"></span>');
       }
-      position += 1;
     }
 
     console.log('  </div>');
