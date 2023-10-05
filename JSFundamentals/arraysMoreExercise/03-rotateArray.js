5fbda6df0d279ca4e2f5eb2f566ff942bf7c9e4d
Dimitar Ivanov <riseabove92@yahoo.com>
2023-10-05 18:29:36 +0300

    array more exercises modified file

--- JSFundamentals/arraysMoreExercise/03-rotateArray.js
+++ JSFundamentals/arraysMoreExercise/03-rotateArray.js
@@ -1,10 +1,10 @@
 function rotateArray(arr) {
-  let rotations = Number(arr[arr.length - 1]);
-  arr.pop();
+  let rotations = Number(arr.pop());
+
   for (let i = 0; i < rotations; i++) {
-    let shiftedElement = arr.shift(arr[i]);
-    arr.push(shiftedElement);
+    let lastElement = arr.pop();
+    arr.unshift(lastElement);
   }
   console.log(arr.join(' '));
 }
-rotateArray(['1', '2', '3', '4', '2']);
+rotateArray(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
