931b79966b9ad2ef57d84f5306725aa9e48098d9
Dimitar Ivanov <riseabove92@yahoo.com>
2023-10-09 13:26:18 +0300

    function exercise

--- JSFundamentals/functionExercise/03-charactersInRange.js
+++ JSFundamentals/functionExercise/03-charactersInRange.js
@@ -1,7 +1,16 @@
 function charInRange(char1, char2) {
   let character1 = char1.charCodeAt(0);
   let character2 = char2.charCodeAt(0);
-  console.log(character1);
-  console.log(character2);
+
+  let minCode = Math.min(character1, character2);
+  let maxCode = Math.max(character1, character2);
+
+  let result = '';
+  for (let i = minCode + 1; i < maxCode; i++) {
+    let curChar = String.fromCharCode(i);
+    result += curChar + ' ';
+  }
+
+  console.log(result);
 }
-charInRange('a', 'd');
+charInRange('#', ':');
