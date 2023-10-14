7040d5c38bc2c0402fa6f524ff07eb21d42c574c
Dimitar Ivanov <riseabove92@yahoo.com>
2023-10-14 08:28:14 +0300

    array advanced exercise

--- JSFundamentals/arraysAdvancedLab/03-firstAndLastK.js
+++ JSFundamentals/arraysAdvancedLab/03-firstAndLastK.js
@@ -1,2 +1,7 @@
-function firstAndLastK(arr) {}
+function firstAndLastK(arr) {
+  let firstElement = arr.shift();
+
+  console.log(arr.slice(0, firstElement).join(' '));
+  console.log(arr.slice(-firstElement).join(' '));
+}
 firstAndLastK([2, 7, 8, 9]);
