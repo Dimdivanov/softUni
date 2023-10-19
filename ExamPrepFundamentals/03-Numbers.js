40c7005259001f4ea58ae49dd49e9a8ef75fa0ec
Dimitar Ivanov <riseabove92@yahoo.com>
2023-10-19 16:22:37 +0300

    exam prep

--- ExamPrepFundamentals/03-Numbers.js
+++ ExamPrepFundamentals/03-Numbers.js
@@ -2,9 +2,9 @@ function numbers(str) {
   let nums = str.split(' ').map(Number);
   let avg = nums.reduce((acc, val) => acc + val) / nums.length;
   let topNums = nums
-    .filter((num) => num > avg)
-    .sort((a, b) => a - b)
-    .slice(0, 5);
+    .filter((num) => num > avg) //филтрирай числата които само по големи от средно аритметичното
+    .sort((a, b) => b - a) // сортирай ги по големина (обърнати)
+    .slice(0, 5); // вземи само тези елементи
 
   if (topNums.length == 0) {
     console.log('No');
