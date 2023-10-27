cb5aff6b09929f9f935ab40d156331256f6fdbef
Dimitar Ivanov <riseabove92@yahoo.com>
2023-10-27 14:01:33 +0300

    objects and classes lab

---
+++ JSFundamentals/objectsAndClasses/01-personInfo.js
@@ -0,0 +1,12 @@
+function solve(firstName, lastName, age) {
+  let person = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+  };
+  console.log(person.age);
+  console.log(person['age']);
+
+  return person;
+}
+solve('Peter', 'Pan', '20');
