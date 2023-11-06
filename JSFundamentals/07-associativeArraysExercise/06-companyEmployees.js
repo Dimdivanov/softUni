053ad498137049118ceea8631a02cbc7a64937b0
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-06 12:59:34 +0200

    assoc arrays exercise

---
+++ JSFundamentals/07-associativeArraysExercise/06-companyEmployees.js
@@ -0,0 +1,30 @@
+function companyUsers(arr) {
+  let companyObject = {};
+
+  for (let companyEmployee of arr) {
+    let [company, employeeId] = companyEmployee.split(' -> ');
+
+    if (company in companyObject) {
+      if (!companyObject[company].includes(employeeId)) {
+        //ако все още не е вътре в масива на компанията
+        companyObject[company].push(employeeId); //добави елемента в масива на фирмата
+      }
+    } else {
+      companyObject[company] = [employeeId];
+    }
+  }
+  let entries = Object.entries(companyObject).sort((a, b) =>
+    a[0].localeCompare(b[0])
+  );
+
+  for (let [company, ids] of entries) {
+    console.log(`${company}`);
+    ids.forEach((ids) => console.log(`-- ${ids}`));
+  }
+}
+companyUsers([
+  'SoftUni -> AA12345',
+  'SoftUni -> BB12345',
+  'Microsoft -> CC12345',
+  'HP -> BB12345',
+]);
