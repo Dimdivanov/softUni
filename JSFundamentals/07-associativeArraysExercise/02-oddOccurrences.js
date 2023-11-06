50e93d75c78265f04154b53861629bf538141001
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-06 19:42:23 +0200

    assoc array exercise

--- JSFundamentals/07-associativeArraysExercise/02-oddOccurrences.js
+++ JSFundamentals/07-associativeArraysExercise/02-oddOccurrences.js
@@ -1,10 +1,18 @@
 function oddOccurrences(arr) {
-  let words = arr.split(' ');
+  let words = arr.toLowerCase().split(' ');
   let object = {};
 
-  for (let i = 0; i < words.length; i++) {
-    let curElement = words[i];
-    console.log(curElement);
+  for (let word of words) {
+    if (word in object) {
+      object[word]++;
+    } else {
+      object[word] = 1;
+    }
   }
+  let oddOccur = Object.keys(object)
+    .filter((word) => object[word] % 2 === 1)
+    .sort((a, b) => a.localeCompare(b));
+
+  console.log(oddOccur);
 }
 oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
