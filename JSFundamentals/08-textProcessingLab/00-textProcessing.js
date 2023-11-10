ca4532cfac28f482b65e9e2fd32c0c890826a71a
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-10 17:27:21 +0200

    text processing lab

--- JSFundamentals/08-textProcessingLab/00-textProcessing.js
+++ JSFundamentals/08-textProcessingLab/00-textProcessing.js
@@ -4,17 +4,20 @@ let text = 'I am JavaScript developer';
 console.log(text.indexOf('Java')); // output: 5
 console.log(text.indexOf('java')); // output: -1
 console.log(text.indexOf('Java', 4)); // 4 is the index it will start looking for similar substring
-// Extracting substrings
+// =========================================
+// EXTRACTING SUBSTRINGS
 // .substring(starting index, ending index)
 let str = 'Hello world';
 let sub = str.substring(1, 5);
 console.log(sub);
-// String Operation
+// =========================================
+// STRING OPERATIONS
 // .replace(search, replacement)
 let webText = 'SoftUni.bg';
 let replaced = webText.replace('.bg', '.com');
 console.log(replaced);
-// Splitting and finding
+// =========================================
+// SPLITTING AND FINDING
 // .split(operator)
 let text1 = 'I love fruits';
 let wordSplits = text1.split(' love '); // we can split by a word
@@ -22,6 +25,9 @@ console.log(wordSplits);
 // .includes(substring)
 let text2 = 'I love fruits';
 console.log(text2.includes('fruits')); //output: true (only true/false)
+// =========================================
 // .repeat(count) - creates a new string repeated count times
 let names = 'Dimitar ';
 console.log(names.repeat(2));
+// =========================================
+// TRIMMING STRINGS
