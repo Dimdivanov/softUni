b809e812f75810cba4ef390403b0efc796252c0d
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-10 16:57:40 +0200

    text processing lab

--- JSFundamentals/08-textProcessingLab/00-textProcessing.js
+++ JSFundamentals/08-textProcessingLab/00-textProcessing.js
@@ -1,5 +1,5 @@
 // Manipulation
-//indexOf(substring)
+// .indexOf(substring) - gives index of the searched string
 let text = 'I am JavaScript developer';
 console.log(text.indexOf('Java')); // output: 5
 console.log(text.indexOf('java')); // output: -1
@@ -14,3 +14,14 @@ console.log(sub);
 let webText = 'SoftUni.bg';
 let replaced = webText.replace('.bg', '.com');
 console.log(replaced);
+// Splitting and finding
+// .split(operator)
+let text1 = 'I love fruits';
+let wordSplits = text1.split(' love '); // we can split by a word
+console.log(wordSplits);
+// .includes(substring)
+let text2 = 'I love fruits';
+console.log(text2.includes('fruits')); //output: true (only true/false)
+// .repeat(count) - creates a new string repeated count times
+let names = 'Dimitar ';
+console.log(names.repeat(2));
