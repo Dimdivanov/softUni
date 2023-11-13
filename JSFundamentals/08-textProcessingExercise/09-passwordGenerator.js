2b7ac5f7ff6b3642c0e3c0e338f78614e149ebf8
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-13 18:14:53 +0200

    text processing exercise

---
+++ JSFundamentals/08-textProcessingExercise/09-passwordGenerator.js
@@ -0,0 +1,21 @@
+function passWordGenerator(arr) {
+  let [str1, str2, word] = arr;
+  let concatStr = str1 + str2;
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let idx = 0;
+  for (let ch of concatStr) {
+    if (vowels.includes(ch)) {
+      concatStr = concatStr.replace(ch, word[idx].toUpperCase());
+      idx++;
+      if (idx == word.length) {
+        idx = 0;
+      }
+    }
+  }
+  console.log(
+    `Your generated password is ${concatStr.split('').reverse().join('')}`
+  );
+}
+passWordGenerator(['ilovepizza', 'ihatevegetables', 'orange']);
+
+// the vowels are a, e, i, o, and u
