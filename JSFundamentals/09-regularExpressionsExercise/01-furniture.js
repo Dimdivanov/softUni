ab6361c7d0e90d50c2c9149dfa5846fecb141380
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-18 14:52:31 +0200

    regular expressions exercise

---
+++ JSFundamentals/09-regularExpressionsExercise/01-furniture.js
@@ -0,0 +1,27 @@
+function furniture(input) {
+  let myArr = [];
+  let totalPrice = 0;
+
+  let pattern = />>(?<name>[A-Z][A-Za-z]+)<<(?<price>\d+\.?\d*)!(?<qty>\d+)/;
+
+  let command = input.shift();
+
+  while (command !== 'Purchase') {
+    let match = command.match(pattern);
+
+    if (match) {
+      let { name, price, qty } = match.groups;
+      let furniturePrice = Number(qty) * Number(price);
+      myArr.push(name);
+      totalPrice += furniturePrice;
+    }
+
+    command = input.shift();
+  }
+  console.log('Bought furniture:');
+  if (myArr.length > 0) {
+    console.log(myArr.join('\n'));
+  }
+  console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
+}
+furniture(['>>Sofa<<312.23!3', '>>TV<<300!5', '>Invalid<<!5', 'Purchase']);
