6f745fef5d683403196d22137555f38b8dc9c82e
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-23 15:50:19 +0200

    associative arrays exercise

--- JSFundamentals/07-associativeArraysExercise/10-legendaryFarming.js
+++ JSFundamentals/07-associativeArraysExercise/10-legendaryFarming.js
@@ -1,49 +1,53 @@
 function legendaryFarming(str) {
-  let legendaries = { shards: 0, fragments: 0, motes: 0 };
-  let junkItems = {};
+  let keyMaterials = { shards: 0, fragments: 0, motes: 0 };
+  let junkMaterials = {};
 
-  let legos = {
+  let legendaries = {
     shards: 'Shadowmourne',
     fragments: 'Valanyr',
     motes: 'Dragonwrath',
   };
 
-  let materials = str.split(' ');
+  let materialsArr = str.split(' ');
 
-  for (let i = 0; i < materials.length; i += 2) {
-    let qty = Number(materials[i]);
-    let matName = materials[i + 1].toLowerCase();
+  for (let i = 0; i < materialsArr.length; i += 2) {
+    let qty = Number(materialsArr[i]);
+    let material = materialsArr[i + 1].toLowerCase();
 
-    if (matName in legendaries) {
-      legendaries[matName] += qty;
-      if (legendaries[matName] >= 250) {
-        let legendaryWon = legos[matName];
+    if (material in keyMaterials) {
+      keyMaterials[material] += qty;
+
+      if (keyMaterials[material] >= 250) {
+        let legendaryWon = legendaries[material];
         console.log(`${legendaryWon} obtained!`);
-        legendaries[matName] -= 250;
+
+        keyMaterials[material] -= 250;
         break;
       }
     } else {
-      if (matName in junkItems) {
-        junkItems[matName] += qty;
+      if (material in junkMaterials) {
+        junkMaterials[material] += qty;
+      } else {
+        junkMaterials[material] = qty;
       }
-      junkItems[matName] = qty;
     }
   }
 
-  let matEntries = Object.entries(legendaries).sort(
+  let keyMaterialEntries = Object.entries(keyMaterials).sort(
     (a, b) => b[1] - a[1] || a[0].localeCompare(b[0])
   );
-  for (let [name, quant] of matEntries) {
+  for (let [name, quant] of keyMaterialEntries) {
     console.log(`${name}: ${quant}`);
   }
-  let junkEntries = Object.entries(junkItems).sort(
-    (a, b) => b[1] - a[1] || a[0].localeCompare(b[0])
+
+  let junkMaterialEntries = Object.entries(junkMaterials).sort((a, b) =>
+    a[0].localeCompare(b[0])
   );
-  for (let [junkName, junkQty] of junkEntries) {
-    console.log(`${junkName}: ${junkQty}`);
+  for (let [name, quant] of junkMaterialEntries) {
+    console.log(`${name}: ${quant}`);
   }
 }
 legendaryFarming('3 Motes 5 stones 5 Shards 6 leathers 255 fragments 7 Shards');
-// legendaryFarming(
-//   '123 silver 6 shards 8 shards 5 motes 9 fangs 75 motes 103 MOTES 8 Shards 86 Motes 7 stones 19 silver'
-// );
+legendaryFarming(
+  '123 silver 6 shards 8 shards 5 motes 9 fangs 75 motes 103 MOTES 8 Shards 86 Motes 7 stones 19 silver'
+);
