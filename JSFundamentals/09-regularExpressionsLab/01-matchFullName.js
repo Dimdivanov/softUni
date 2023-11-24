7ff65186ab72101c0c59c99da4563be0f84fb527
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-24 16:16:57 +0200

    regExp lab

--- JSFundamentals/09-regularExpressionsLab/01-matchFullName.js
+++ JSFundamentals/09-regularExpressionsLab/01-matchFullName.js
@@ -1,7 +1,7 @@
 function matchName(input) {
   let pattern = /\b([A-Z]{1}[a-z]+) ([A-Z]{1}[a-z]+)\b/g;
   let match = input.match(pattern);
-  console.log(match);
+  console.log(match.join(' '));
 }
 matchName(
   'Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov'
