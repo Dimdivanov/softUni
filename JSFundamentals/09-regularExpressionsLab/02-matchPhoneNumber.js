0c77093c63739d0b09f2661b09b84c74db9f8e81
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-24 16:14:40 +0200

    regExp lab

---
+++ JSFundamentals/09-regularExpressionsLab/02-matchPhoneNumber.js
@@ -0,0 +1,12 @@
+function matchNumber(input) {
+  let validNames = [];
+  let pattern = /(?<!\d)[+]359([ -])2\1\d{3}\1\d{4}\b/g;
+  while ((validName = pattern.exec(input)) !== null) {
+    validNames.push(validName[0]);
+  }
+  console.log(validNames.join(', '));
+}
+
+matchNumber([
+  '+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222',
+]);
