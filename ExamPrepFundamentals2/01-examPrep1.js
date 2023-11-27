ab3405e39afd789494285e1cd563bf270b305f4e
Dimitar Ivanov <riseabove92@yahoo.com>
2023-11-27 19:29:20 +0200

    Final Exam Prep 1

---
+++ ExamPrepFundamentals2/01-examPrep1.js
@@ -0,0 +1,43 @@
+function secretChat(input) {
+  let msg = input.shift(); //heVVodar!gniV
+  let command = input.shift();
+
+  while (command != 'Reveal') {
+    let curCommand = command.split(':|:');
+    let action = curCommand.shift(); //changeAll, Reverse, insert
+
+    if (action == 'InsertSpace') {
+      //inserting spaces
+      let index = Number(curCommand.shift());
+      msg = msg.slice(0, index) + ' ' + msg.slice(index);
+      console.log(msg);
+    } else if (action == 'Reverse') {
+      let str = curCommand.shift();
+      if (msg.includes(str)) {
+        let reversedStr = str.split('').reverse().join('');
+        msg = msg.replace(str, '') + reversedStr;
+        console.log(msg);
+      } else {
+        console.log('error');
+      }
+    } else if (action == 'ChangeAll') {
+      let [str1, str2] = curCommand;
+      while (msg.includes(str1)) {
+        msg = msg.replace(str1, str2);
+      }
+      console.log(msg);
+    }
+
+    command = input.shift();
+  }
+  if (command == 'Reveal') {
+    console.log(`You have a new text message: ${msg}`);
+  }
+}
+secretChat([
+  'heVVodar!gniV',
+  'ChangeAll:|:V:|:l',
+  'Reverse:|:!gnil',
+  'InsertSpace:|:5',
+  'Reveal',
+]);
