0e58a3875f043c9883b1d8d22536b60dfcb58305
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-17 11:56:00 +0200

    Arrays and Nested Arrays Exercise

---
+++ JSAdvanced/04-arraysAndNestedArraysExercise/03-addRemoveElementsV3reduce.js
@@ -0,0 +1,5 @@
+function addRemoveElements(command) {}
+
+addRemoveElements(['add', 'add', 'add', 'add']);
+addRemoveElements(['add', 'add', 'remove', 'add', 'add']);
+addRemoveElements(['remove', 'remove', 'remove']);
