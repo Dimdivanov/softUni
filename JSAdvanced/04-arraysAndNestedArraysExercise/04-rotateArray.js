925674abdbcb3c9231de5c2d90003177433f99f0
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-17 14:16:52 +0200

    Arrays and Nested Arrays Exercise

--- JSAdvanced/04-arraysAndNestedArraysExercise/04-rotateArray.js
+++ JSAdvanced/04-arraysAndNestedArraysExercise/04-rotateArray.js
@@ -1,2 +1,9 @@
-function rotateArr(arr, n) {}
-rotateArr();
+function rotateArr(arr, n) {
+  for (let i = 0; i < n; i++) {
+    let moved = arr.pop();
+    arr.unshift(moved);
+  }
+  console.log(arr.join(' '));
+}
+rotateArr(['1', '2', '3', '4'], 2);
+rotateArr(['Banana', 'Orange', 'Coconut', 'Apple'], 15);
