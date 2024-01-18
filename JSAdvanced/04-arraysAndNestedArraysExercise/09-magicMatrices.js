d2c1cd7c8ad620ac2cc253420522eb0fd46d6b5d
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-18 22:45:48 +0200

    Arrays and Nested Arrays Exercise

--- JSAdvanced/04-arraysAndNestedArraysExercise/09-magicMatrices.js
+++ JSAdvanced/04-arraysAndNestedArraysExercise/09-magicMatrices.js
@@ -1,7 +1,21 @@
 function magicMatrices(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let column = arr[i][i];
+  for (let i = 0; i < arr.length - 1; i++) {
+    let rowOne = arr.reduce((acc, value, index) => {
+      acc += value[i];
+      return acc;
+    }, 0);
+    let rowTwo = arr.reduce((acc, value, index) => {
+      acc += value[i + 1];
+      return acc;
+    }, 0);
+    let colOne = arr[i].reduce((acc, value) => acc + value);
+    let colTwo = arr[i + 1].reduce((acc, value) => acc + value);
+
+    if (rowOne !== rowTwo || colOne !== colTwo) {
+      return false;
+    }
   }
+  return true;
 }
 magicMatrices([
   [4, 5, 6],
