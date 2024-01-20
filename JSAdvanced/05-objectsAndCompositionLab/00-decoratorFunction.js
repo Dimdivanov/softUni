f13c0d773b0b1f063e31aed4a74a167a4cf0728d
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-20 14:34:51 +0200

    Objects and Composition Exercise

--- JSAdvanced/05-objectsAndCompositionLab/00-decoratorFunction.js
+++ JSAdvanced/05-objectsAndCompositionLab/00-decoratorFunction.js
@@ -1,10 +1,12 @@
+//Can add new Data and behavior to already existing object
 function canPrint(device) {
+  // 2/4creates the behavior
   device.print = () => {
     console.log(`${device.name} is printing a page`);
   };
 }
 
-const printer = { name: 'Acme printer' };
-canPrint(printer);
+const printer = { name: 'Acme printer' }; // 1/4 object is existing
+canPrint(printer); // 3/4 calls the function on top with the device = printer
 
-printer.print();
+printer.print(); // 4/4 calls the newly created func in the object
--- JSAdvanced/05-objectsAndCompositionLab/00-factoryFunction.js
+++ JSAdvanced/05-objectsAndCompositionLab/00-factoryFunction.js
@@ -1,4 +1,5 @@
-//creates objects in a way that allows for encapsulation and creation of multiple instances
+//creates objects and behavior in a way that allows
+//for encapsulation and creation of multiple instances
 function createRect(width, height) {
   const rect = { width, height }; //creates an object
   //creates behavior
@@ -12,3 +13,17 @@ let rectangle2 = createRect(7, 8);
 
 console.log(rectangle1);
 console.log(rectangle2);
+
+//Example 2
+/* function factory(library, orders) {
+const result = [];
+for (let order of orders) {
+// Create a copy of the 'template' object from the current order
+const current = Object.assign({}, order.template);
+for (let part of order.parts) {
+current[part] = library[part];
+}
+result.push(current);
+}
+return result;
+} */
