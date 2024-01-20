c8548df3fab66bc5afb6665fe13f448fbd0900a3
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-20 14:13:11 +0200

    JS - Advanced Objects and Composition Lab

--- JSAdvanced/05-objectsAndCompositionLab/00-factoryFunction.js
+++ JSAdvanced/05-objectsAndCompositionLab/00-factoryFunction.js
@@ -1,10 +1,12 @@
 function createRect(width, height) {
   const rect = { width, height };
-
   rect.getArea = () => {
     return rect.width * rect.height;
   };
-
   return rect;
 }
-createRect(4, 5);
+let rectangle1 = createRect(4, 5);
+let rectangle2 = createRect(7, 8);
+
+console.log(rectangle1);
+console.log(rectangle2);
