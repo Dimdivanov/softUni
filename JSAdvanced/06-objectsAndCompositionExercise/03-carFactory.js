0d25d754c17e413589638dd9d570dc52d0fa46cd
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-20 16:21:22 +0200

    Objects and Composition Exercise

---
+++ JSAdvanced/06-objectsAndCompositionExercise/03-carFactory.js
@@ -0,0 +1,33 @@
+function carFactory(order) {
+  let result = {
+    model: order.model,
+    engine: null,
+    carriage: null,
+    wheelsize: [],
+  };
+
+  if (order.power <= 90) {
+    result.engine = { power: 90, volume: 1800 };
+  } else if (order.power <= 120) {
+    result.engine = { power: 120, volume: 2400 };
+  } else {
+    result.engine = { power: 200, volume: 3500 };
+  }
+  if (order.carriage == 'hatchback') {
+    result.carriage = { type: 'hatchback', color: order.color };
+  } else {
+    result.carriage = { type: 'coupe', color: order.color };
+  }
+  if (order.wheelsize % 2 === 0) {
+    order.wheelsize = order.wheelsize - 1;
+    result.wheelsize = Array(4).fill(order.wheelsize);
+  }
+  return result;
+}
+carFactory({
+  model: 'VW Golf II',
+  power: 90,
+  color: 'blue',
+  carriage: 'hatchback',
+  wheelsize: 14,
+});
