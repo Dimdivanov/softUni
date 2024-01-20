fb42cae5ad1def957a457adbdecb73561c853fcc
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-20 14:17:01 +0200

    JS - Advanced Objects and Composition Lab

--- JSAdvanced/05-objectsAndCompositionLab/00-factoryFunction.js
+++ JSAdvanced/05-objectsAndCompositionLab/00-factoryFunction.js
@@ -1,11 +1,13 @@
+//creates objects in a way that allows for encapsulation and creation of multiple instances
 function createRect(width, height) {
-  const rect = { width, height };
+  const rect = { width, height }; //creates an object
+  //creates behavior
   rect.getArea = () => {
     return rect.width * rect.height;
   };
   return rect;
 }
-let rectangle1 = createRect(4, 5);
+let rectangle1 = createRect(4, 5); //to avoid using constructor function and 'new' keyword
 let rectangle2 = createRect(7, 8);
 
 console.log(rectangle1);
