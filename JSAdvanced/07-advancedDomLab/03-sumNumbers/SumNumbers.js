9ceea5231bc8f1367280c7fb982f24654ec7bca0
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-22 20:07:30 +0200

    DOM Introduction lab

--- JSAdvanced/07-advancedDomLab/03-sumNumbers/SumNumbers.js
+++ JSAdvanced/07-advancedDomLab/03-sumNumbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+  let num1 = document.getElementById('num1'); //target bar1
+  let num2 = document.getElementById('num2'); //target bar2
+
+  let result = document.getElementById('sum');
+  result.value = Number(num1.value) + Number(num2.value);
 }
