5e11989099335982dfb47e5e1fce5b2b6ae4a766
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-22 17:12:56 +0200

    DOM Introduction lab

--- JSAdvanced/07-advancedDomLab/02-collectListItems/CollectListItems.js
+++ JSAdvanced/07-advancedDomLab/02-collectListItems/CollectListItems.js
@@ -1,12 +1,13 @@
 function extractText() {
-  let list = document.getElementById('items');
-  let result = [];
+  let result = []; // масив да държи резултата
+  let list = document.getElementById('items'); // намираме елемента И СЕЛЕКТИРАМЕ
 
-  let items = Array.from(list.children);
+  let items = Array.from(list.children); // взимаме неговите деца
   for (let item of items) {
-    result.push(item.textContent);
+    // минаваме през всеки от елементите ЧЕТЕМ
+    result.push(item.textContent); // текст контента на всеки елемент го запазваме
   }
 
-  let textArea = document.getElementById('result');
-  textArea.value = result.join('\n');
+  let textArea = document.getElementById('result'); // намираме на страницата къде ще е резултата СЕЛЕКТИРАМЕ
+  textArea.value = result.join('\n'); // сетваме като value на конкретния резултат ПРОМЕНЯМЕ стойността
 }
