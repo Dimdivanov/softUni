04807ebf0c03c60cf9a8bdb5f01cfa7999d06373
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-23 16:17:58 +0200

    DOM Introduction lab

--- JSAdvanced/07-advancedDomLab/07-extractParenthesis/ExtractParenthesis.js
+++ JSAdvanced/07-advancedDomLab/07-extractParenthesis/ExtractParenthesis.js
@@ -1,3 +1,11 @@
-function extract(content) {
+function extract(elementId) {
+  let contentRef = document.getElementById(elementId).textContent;
+  let pattern = /\((.*?)\)/g;
+  let result = [];
 
+  let match = contentRef.match(pattern);
+  for (let el of match) {
+    result.push(el);
+  }
+  return result.join('; ');
 }
