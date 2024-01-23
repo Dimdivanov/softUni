c1590bd7e4b25dd95c7ab244c55be8c14cdee993
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-23 10:32:35 +0200

    DOM Introduction lab

---
+++ JSAdvanced/07-advancedDomLab/04-showMore/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+  let targetShow = document.getElementById('more');
+  let textDisplay = document.getElementById('text');
+
+  textDisplay.style.display = 'inline';
+  targetShow.style.display = 'none';
+}
