50786a382a1f19215f0c94f5b2cfebc7a3fe22cc
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-24 16:18:27 +0200

    DOM Introduction Exercise

--- JSAdvanced/08-DomIntroExercise/05. Table - Search Engine/solution.js
+++ JSAdvanced/08-DomIntroExercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+  let searchRef = document.getElementById('searchField');
+  let rowDataRef = Array.from(document.querySelectorAll('tbody tr'));
 
-   }
+  function onClick() {
+    let searchText = searchRef.value;
+    //loop through the cells minus 1st line and check for full words or letters
+    for (let tableRow of rowDataRef) {
+      let tableData = Array.from(tableRow.querySelectorAll('td'));
+      for (let data of tableData) {
+        if (data.textContent.includes(searchText)) {
+          tableRow.classList.add('select');
+          break;
+        } else {
+          tableRow.classList.remove('select');
+        }
+      }
+      searchRef.value = '';
+    }
+  }
 }
