3ba984980afa597bc8ab954ac7422d2fdb5c5b84
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-24 15:26:33 +0200

    DOM Introduction Exercise

--- JSAdvanced/08-DomIntroExercise/04. Search in List/search.js
+++ JSAdvanced/08-DomIntroExercise/04. Search in List/search.js
@@ -1,3 +1,16 @@
 function search() {
-   // TODO
+  let townsArr = Array.from(document.querySelectorAll('#towns li'));
+  let searched = document.getElementById(`searchText`).value;
+  let matches = 0;
+
+  for (let town of townsArr) {
+    if (town.textContent.includes(searched)) {
+      town.style.textDecoration = `underline`;
+      town.style.fontWeight = `bold`;
+      matches++;
+    }
+  }
+
+  document.getElementById(`searchText`).value = '';
+  document.getElementById('result').textContent = `${matches} matches found`;
 }
