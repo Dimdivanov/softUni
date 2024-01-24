77c0b188a4de662490bc59ca9fae69bb5f5aa8c0
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-24 13:29:15 +0200

    DOM Introduction Exercise

--- JSAdvanced/08-DomIntroExercise/03. Accordion/accordion.js
+++ JSAdvanced/08-DomIntroExercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+  let buttonRef = document.getElementsByClassName('button')[0];
+  let hiddenDivRef = document.getElementById('extra');
+  let text = buttonRef.textContent;
+
+  if (text == 'More') {
+    hiddenDivRef.style.display = 'block';
+    buttonRef.textContent = 'Less';
+  }
+  if (text == 'Less') {
+    hiddenDivRef.style.display = 'none';
+    buttonRef.textContent = 'More';
+  }
 }
