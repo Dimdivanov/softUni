114516dd89ab98d07af3a44475b37a12bf0caf64
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-24 19:05:40 +0200

    DOM Introduction Exercise

--- JSAdvanced/08-DomIntroExercise/06. Format the Text/solution.js
+++ JSAdvanced/08-DomIntroExercise/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  let inputAreaRef = document.getElementById('input');
+  let outputAreaRef = document.getElementById('output');
+
+  let text = inputAreaRef.value;
+  let arrayOfText = text.split('. ');
+
+  let result = '';
+  for (let i = 0; i < arrayOfText.length; i++) {
+    if (i % 3 === 0) {
+      result += '<p>';
+    }
+    result += arrayOfText[i];
+    if ((i + 1) % 3 === 0 || i === arrayOfText.length - 1) {
+      result += '</p>';
+    } else {
+      result += '. ';
+    }
+  }
+
+  outputAreaRef.innerHTML += `${result}`;
 }
