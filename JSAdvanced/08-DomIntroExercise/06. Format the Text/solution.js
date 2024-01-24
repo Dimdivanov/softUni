44b1347c5e65744a39883fdd40131b15972d4844
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-24 19:43:39 +0200

    DOM Introduction Exercise

--- JSAdvanced/08-DomIntroExercise/06. Format the Text/solution.js
+++ JSAdvanced/08-DomIntroExercise/06. Format the Text/solution.js
@@ -4,19 +4,39 @@ function solve() {
 
   let text = inputAreaRef.value;
   let arrayOfText = text.split('. ');
-
-  let result = '';
-  for (let i = 0; i < arrayOfText.length; i++) {
-    if (i % 3 === 0) {
-      result += '<p>';
+  if (text.trim() !== '') {
+    // if empty space doesnt create new <p>
+    let result = '';
+    for (let i = 0; i < arrayOfText.length; i++) {
+      if (i % 3 === 0) {
+        result += '<p>';
+      }
+      result += arrayOfText[i];
+      if ((i + 1) % 3 === 0 || i === arrayOfText.length - 1) {
+        result += '</p>';
+      } else {
+        result += '. ';
+      }
     }
-    result += arrayOfText[i];
-    if ((i + 1) % 3 === 0 || i === arrayOfText.length - 1) {
-      result += '</p>';
-    } else {
-      result += '. ';
-    }
+    outputAreaRef.innerHTML += `${result}`;
   }
-
-  outputAreaRef.innerHTML += `${result}`;
 }
+
+//100 / 100
+// function solve() {
+//   let text = document
+//     .getElementById('input')
+//     .value.split('.')
+//     .filter((e) => e.length > 0);
+//   let div = document.getElementById('output');
+//   for (i = 0; i < text.length; i += 3) {
+//     let output = [];
+//     for (j = 0; j < 3; j++) {
+//       if (text[i + j]) {
+//         output.push(text[i + j]);
+//       }
+//     }
+//     let res = output.join('. ') + '.';
+//     div.innerHTML += `<p>${res}</p>`;
+//   }
+// }
