b54a810d5afac0eb0f0d2ba22d30b00de9d95178
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-25 13:02:16 +0200

    DOM Events Lab

--- JSAdvanced/09-DomEventsLab/01. List-Of-Items/app.js
+++ JSAdvanced/09-DomEventsLab/01. List-Of-Items/app.js
@@ -1,1 +1,11 @@
-function addItem() {}
+function addItem() {
+  let input = document.getElementById('newItemText'); //взимаме от тук input
+  if (input.value.length == 0) {
+    return;
+  }
+  let liElement = document.createElement('li'); //правим празен елемент 'li'
+  liElement.textContent = input.value; // даваме на елемента стойност
+  let list = document.getElementById('items'); // избираме къде ще слагаме
+  list.appendChild(liElement); // апендване на елемента
+  input.value = ''; //зануляваме инпута
+}
--- JSAdvanced/09-DomEventsLab/02. Delete-From-Table/app.js
+++ JSAdvanced/09-DomEventsLab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let inputRef = document.querySelector('[name="email"]').value;
+  let tableRowRef = Array.from(document.querySelectorAll('tr'));
+  let result = document.getElementById('result');
+  for (let row of tableRowRef) {
+    let td = Array.from(row.children);
+    for (let i = 1; i < td.length; i++) {
+      let element = td[i];
+      if (element.textContent.includes(inputRef)) {
+        row.remove();
+        result.textContent = 'Deleted';
+        return;
+      }
+    }
+  }
+  result.textContent = 'Not found.';
 }
