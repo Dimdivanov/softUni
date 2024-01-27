60227a266ae34970982b7f3a57ab9d6589745dde
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-27 10:47:14 +0200

    DOM Events Lab

--- JSAdvanced/09-DomEventsLab/04. Mouse-Gradient/app.js
+++ JSAdvanced/09-DomEventsLab/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  let gradient = document.getElementById('gradient');
+  gradient.addEventListener('mousemove', gradMove);
+  gradient.addEventListener('mouseout', gradOut);
+
+  function gradMove(event) {
+    let boxWidth = event.target.clientWidth;
+    let mousePosition = event.offsetX / boxWidth;
+    let percentage = Math.trunc(mousePosition * 100);
+    document.getElementById('result').textContent = percentage + '%';
+  }
+  function gradOut() {
+    document.getElementById('result').textContent = '';
+  }
 }
