333828cc37a0be21d561a0962714baf1d7165006
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-27 09:40:33 +0200

    DOM Intro Exercise

--- JSAdvanced/08-DomIntroExercise/08-Generate Report/report.js
+++ JSAdvanced/08-DomIntroExercise/08-Generate Report/report.js
@@ -1,4 +1,32 @@
 function generateReport() {
-  //factory function that give us an object
-  //stringify to JSON
+  let inputElements = Array.from(document.getElementsByTagName('input'));
+  let tableRows = Array.from(document.getElementsByTagName('tr'));
+
+  const resultArr = [];
+  const checkedCols = [];
+
+  for (let i = 0; i < tableRows.length; i++) {
+    const row = tableRows[i];
+    const obj = {};
+
+    for (let k = 0; k < row.children.length; k++) {
+      const element = row.children[k];
+      if (i == 0) {
+        if (element.children[0].checked) {
+          checkedCols.push(k);
+        }
+        continue;
+      }
+
+      if (checkedCols.includes(k)) {
+        let propertyName = inputElements[k].name;
+        obj[propertyName] = element.textContent;
+      }
+    }
+    if (i !== 0) {
+      resultArr.push(obj);
+    }
+  }
+
+  document.getElementById('output').value = JSON.stringify(resultArr);
 }
--- JSAdvanced/09-DomEventsLab/03. Add-Delete/app.js
+++ JSAdvanced/09-DomEventsLab/03. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    
+  let input = document.getElementById('newItemText'); //взимаме от тук input
+  if (input.value.length == 0) {
+    return;
+  }
+  let liElement = document.createElement('li'); //правим празен елемент 'li'
+  liElement.textContent = input.value; // даваме на елемента стойност
+
+  let deleteBtn = document.createElement('a');
+  deleteBtn.textContent = '[Delete]';
+  deleteBtn.href = '#';
+  deleteBtn.addEventListener('click', myFunc);
+  liElement.appendChild(deleteBtn); //ще влезе в liElement-а и ще създаде 'a'
+
+  let list = document.getElementById('items'); // избираме къде ще слагаме
+  list.appendChild(liElement); // апендване на елемента
+  input.value = ''; //зануляваме инпута
+  function myFunc(event) {
+    let deleteBtn = event.target;
+    let liElement = deleteBtn.parentElement;
+    liElement.remove();
+  }
 }
