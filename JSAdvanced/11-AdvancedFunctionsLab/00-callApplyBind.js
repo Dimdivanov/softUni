79f6c48991a7a7496d4effe3b1e789a1631edb39
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-30 14:04:18 +0200

    Advanced Functions Lab

---
+++ JSAdvanced/11-AdvancedFunctionsLab/00-callApplyBind.js
@@ -0,0 +1,36 @@
+//call()
+function sayHello(subject, grade) {
+  console.log(this.name + ' says hi!');
+  console.log(this.name + ' received a ' + grade + ' at ' + subject);
+}
+sayHello('math', 5); //undefined
+console.log('==== Приема обекта(this), и другите параметри изброени ====');
+
+const myObj = {
+  name: 'Peter',
+};
+const mySecondObj = {
+  name: 'Ivan',
+};
+sayHello.call(myObj, 'biology', 6);
+console.log('==== Приема обекта(this), и масив от параметрите ====');
+//apply()
+sayHello.apply(mySecondObj, ['geography', 5]);
+//често срещано приложение на apply е:
+console.log(Math.max.apply(null, [1, 2, 3, 4, 5, 6])); //6
+console.log(
+  '==== запазваме в променлива и приема обекта(this), и прави прозрачна опаковка ===='
+);
+//bind()
+//задава контекст на функцията и я изпълняваме по-нататък
+//няма да изпълни функцията прави като копие
+const boundFn = sayHello.bind(mySecondObj);
+boundFn('art', 4);
+//ако искаме в опаковката да запазим и параметрите можем да направим така
+const boundFn1 = sayHello.bind(mySecondObj, 'art');
+boundFn1(6);
+// behind the work of bind():
+// const boundFnn = function () {
+//   sayHello.call(mySecondObj, 'art', 4);
+// };
+// boundFnn();
---
+++ JSAdvanced/11-AdvancedFunctionsLab/02-add.js
@@ -0,0 +1,9 @@
+function solution(num) {
+  return function (addNum) {
+    return num + addNum;
+  };
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
