791c611bcb21f614cfb8f3bb483932b44a11ade2
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-30 15:41:45 +0200

    Advanced Functions Lab

--- JSAdvanced/11-AdvancedFunctionsLab/00-functionalProgramming.js
+++ JSAdvanced/11-AdvancedFunctionsLab/00-functionalProgramming.js
@@ -23,8 +23,8 @@ const calculate = function (a, b) {
 };
 console.log(calculate(2, 3));
 
-//Higher order Functions
-//take other functions as an ARGUMENT or RETURN a function as a result
+//Както наричаме фунцкиите в обектите(методи) така и тук:
+//=Higher order function- take other functions as an ARGUMENT or RETURN a function as a result
 const sayHello2 = function () {
   return function () {
     console.log('HelloLOL!');
@@ -32,3 +32,26 @@ const sayHello2 = function () {
 };
 const myFunc = sayHello2();
 myFunc();
+//=Predicates - any function that returns bool based evaluation of truth of an assertion
+//== often found in the form of callbacks
+//=== функция която ще върне true или false - filter или find
+//=Built-in higher order functions - map, filter, reduce;
+
+//=Pure Functions
+//== pure functions are functions that always will return the same result with same parameters
+//== value shouldnt have side effects
+//PURE
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(2, 3)); //5
+console.log('======');
+//IMPURE
+let a = 2;
+function add1(b) {
+  return a + b;
+}
+console.log(add1(3)); //5
+a = 3; //changing the variable will change the outcome
+console.log(add1(3)); //6
