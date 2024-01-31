62426244caf69becaee9805c2f9c50d827798dfb
Dimitar Ivanov <riseabove92@yahoo.com>
2024-01-31 14:02:12 +0200

    Advanced Functions Exercise

--- JSAdvanced/12-AdvancedFunctionsExercise/03-fibonacci.js
+++ JSAdvanced/12-AdvancedFunctionsExercise/03-fibonacci.js
@@ -1,1 +1,19 @@
-function fibonacci() {}
+function getFibonator() {
+  let firstNum = 0;
+  let secondNum = 1;
+
+  return function () {
+    let result = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = result;
+    return firstNum;
+  };
+}
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
