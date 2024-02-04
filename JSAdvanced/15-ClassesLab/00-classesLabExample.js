f97daffb4206e253bf17d87a48a75fa77f6d487e
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-04 12:38:38 +0200

    Classes Lab

--- JSAdvanced/15-ClassesLab/00-classesLabExample.js
+++ JSAdvanced/15-ClassesLab/00-classesLabExample.js
@@ -1,8 +1,9 @@
 class Person {
+  //static property - свойства
   static counter = 0;
+
   constructor(name, age, email) {
     Person.counter++;
-
     this.name = name;
     this.age = age;
     this.email = email;
@@ -12,6 +13,12 @@ class Person {
     return `${this.name} ${this.age} ${this.email}`;
   }
 
+  //create a STATIC METHOD that applies only on the class
+  static printCount() {
+    //we access the static counter with this.counter
+    console.log(`${this.counter} instances have been created`);
+  }
+  //we can call another method with this.method()
   static greet() {
     console.log('Say hello!');
     this.bot();
@@ -20,4 +27,10 @@ class Person {
     console.log('Bot');
   }
 }
+Person.printCount();
+let newPerson = new Person('John Smith', 32, 'john@gmail.com');
+let newPerson1 = new Person('Anne Kar', 32, 'anne@gmail.com');
+let newPerson2 = new Person('Peter Jacobson', 32, 'peter@gmail.com');
+
 Person.greet();
+Person.printCount();
