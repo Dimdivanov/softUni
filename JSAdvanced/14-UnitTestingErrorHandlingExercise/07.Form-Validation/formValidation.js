7b6474c287cdb03258edcb59d86fadfad9a8e749
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-07 13:34:27 +0200

    Unit Testing Exercise

--- JSAdvanced/14-UnitTestingErrorHandlingExercise/07.Form-Validation/formValidation.js
+++ JSAdvanced/14-UnitTestingErrorHandlingExercise/07.Form-Validation/formValidation.js
@@ -1,3 +1,82 @@
 function validate() {
-    // TODO
+  let usernamePattern = /^[A-Za-z0-9]{3,20}$/g;
+  let passPattern = /^[\w]{5,15}$/;
+  let emailPattern = /^[^@.]+@[^@]*\.[^@]*$/;
+  let companyNumPatter = /^[1-9\d]{4}$/g;
+
+  let btnSubmit = document.getElementById('submit');
+  let isValid = document.getElementById('valid');
+
+  let userNameRef = document.getElementById('username');
+  let emailRef = document.getElementById('email');
+  let passwordRef = document.getElementById('password');
+  let confirmPasRef = document.getElementById('confirm-password');
+  let companyNumberRef = document.getElementById('companyNumber');
+
+  let isCompanyRef = document.getElementById('company');
+  let companyFieldRef = document.getElementById('companyInfo');
+
+  isCompanyRef.addEventListener('change', companySection);
+  function companySection(e) {
+    if (isCompanyRef.checked) {
+      companyFieldRef.style.display = 'block';
+    } else {
+      companyFieldRef.style.display = 'none';
+      companyNumberRef.value = '';
+    }
+  }
+
+  btnSubmit.addEventListener('click', submitValidator);
+  function submitValidator(e) {
+    e.preventDefault();
+    let truthChecked = true;
+    let userValue = userNameRef.value;
+    let emailValue = emailRef.value;
+    let passValue = passwordRef.value;
+    let confirmValue = confirmPasRef.value;
+    let companyValue = companyNumberRef.value;
+
+    if (userValue.match(usernamePattern)) {
+      userNameRef.style.borderColor = '';
+    } else {
+      truthChecked = false;
+      userNameRef.style.borderColor = 'red';
+    }
+
+    if (emailValue.match(emailPattern)) {
+      emailRef.style.borderColor = '';
+    } else {
+      truthChecked = false;
+      emailRef.style.borderColor = 'red';
+    }
+
+    if (passValue.match(passPattern) && confirmValue === passValue) {
+      passwordRef.style.borderColor = '';
+    } else {
+      truthChecked = false;
+      passwordRef.style.borderColor = 'red';
+    }
+
+    if (confirmValue.match(passPattern) && confirmValue === passValue) {
+      confirmPasRef.style.borderColor = '';
+    } else {
+      truthChecked = false;
+      confirmPasRef.style.borderColor = 'red';
+    }
+
+    if (companyFieldRef.style.display == 'block') {
+      if (companyValue.match(companyNumPatter)) {
+        companyNumberRef.style.borderColor = '';
+      } else {
+        truthChecked = false;
+        companyNumberRef.style.borderColor = 'red';
+      }
+    }
+
+    if (truthChecked) {
+      isValid.style.display = 'block';
+    } else {
+      isValid.style.display = 'none';
+    }
+  }
 }
--- JSAdvanced/15-ClassesLab/00-classesLabExample.js
+++ JSAdvanced/15-ClassesLab/00-classesLabExample.js
@@ -1,10 +1,10 @@
 class Person {
   //static property - свойства
-  static counter = 0;
-  static botCount = 'This is bot counter string';
+  static _counter = 0;
+  static _botCount = 'This is bot counter string';
 
   constructor(name, age, email) {
-    Person.counter++;
+    Person._counter++;
     this.name = name;
     this.age = age;
     this.email = email;
@@ -15,27 +15,26 @@ class Person {
   }
 
   //create a STATIC METHOD that applies only on the class
-  static printCount() {
+  static _printCount() {
     console.log(`${this.counter} instances have been created`);
   }
   //we can call another static method with this.method()
-  static greet() {
+  static _greet() {
     console.log('Say hello!');
     this.bot();
   }
-  static bot() {
+  static _bot() {
     console.log('Bot');
-    console.log(Person.botCount);
-    console.log(this.counter);
+    console.log(Person._botCount);
+    console.log(this._counter);
   }
   //we can clone a person but they wont be equal
-  static clone(person) {
+  static _clone(person) {
     let result = new Person(person.name, person.age, person.email);
     return result;
   }
 }
 
-Person.printCount();
 let newPerson = new Person('John Smith', 32, 'john@gmail.com');
 console.log(newPerson);
 let newPerson2 = new Person('Peter Jacobson', 32, 'peter@gmail.com');
