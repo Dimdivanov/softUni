6c614e5a70e0deae53ef2962ee1e83baa1b69f42
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-09 16:22:43 +0200

    Classes Exercise

--- JSAdvanced/15-ClassesExercise/01-Rectangle-class.js
+++ JSAdvanced/15-ClassesExercise/01-Rectangle-class.js
@@ -1,8 +1,8 @@
 class Rectangle {
   constructor(width, height, color) {
-    (this.width = Number(width)),
-      (this.height = Number(height)),
-      (this.color = color);
+    this.width = Number(width);
+    this.height = Number(height);
+    this.color = color;
   }
   calcArea() {
     return this.width * this.height;
