636e1a3f4b4636b67ee444f19751732ac46ac4c0
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-09 12:42:40 +0200

    Prototype and Inheritance Lab

--- JSAdvanced/16-PrototypeAndInheritance/00-prototypePerson.js
+++ JSAdvanced/16-PrototypeAndInheritance/00-prototypePerson.js
@@ -4,3 +4,12 @@ function Person(first, last, age) {
   this.age = age;
 }
 Person.prototype.nationality = 'Bulgarian';
+let myPerson = new Person('Dimitar', 'Ivanov', 32);
+console.log(myPerson.nationality);
+console.log(myPerson);
+console.log(Person.prototype.nationality);
+
+Person.prototype.nationality = 'Italian';
+
+//взимаме прототипа на новосъздадения обект
+console.log(Person.prototype.nationality);
