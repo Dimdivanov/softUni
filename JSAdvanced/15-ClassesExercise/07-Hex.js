2ee122b49e38a9f4a66c234a7690988089dea175
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-10 11:24:26 +0200

    Classes Exercise

---
+++ JSAdvanced/15-ClassesExercise/07-Hex.js
@@ -0,0 +1,28 @@
+class Hex {
+  constructor(value) {
+    this.value = value;
+  }
+  toString() {
+    return '0x' + this.value.toString(16).toUpperCase();
+  }
+  valueOf() {
+    return this.value;
+  }
+  plus(newHex) {
+    return new Hex(this.value + Number(newHex));
+  }
+  minus(newHex) {
+    return new Hex(this.value - Number(newHex));
+  }
+  parse(str) {
+    return parseInt(str, 16);
+  }
+}
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString() === '0xF');
+console.log(FF.parse('AAA'));
