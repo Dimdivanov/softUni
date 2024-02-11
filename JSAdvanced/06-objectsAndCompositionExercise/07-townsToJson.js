9c6e3afa08f59bc2936d065c1903e2a92c3ed3ad
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-11 18:58:35 +0200

    objects exercise

--- JSAdvanced/06-objectsAndCompositionExercise/07-townsToJson.js
+++ JSAdvanced/06-objectsAndCompositionExercise/07-townsToJson.js
@@ -1,22 +1,26 @@
-function townsToJson(arr) {
-  let newArr = [];
-  let result = {};
-  for (let el of arr) {
-    let tokens = el
+function townsToJSON(input) {
+  const [town, latitude, longitude] = input[0]
+    .split('|')
+    .map((col) => col.trim())
+    .filter(Boolean);
+
+  let result = [];
+
+  for (let i = 1; i < input.length; i++) {
+    const [townName, townLat, townLong] = input[i]
       .split('|')
-      .map((x) => x.trim())
-      .filter((x) => !!x);
-    newArr.push(tokens);
+      .map((col) => col.trim())
+      .filter(Boolean);
+    let townObj = {};
+    townObj[town] = townName;
+    townObj[latitude] = parseFloat(Number(townLat).toFixed(2));
+    townObj[longitude] = parseFloat(Number(townLong).toFixed(2));
+    result.push(townObj);
   }
-  for (let keys of newArr) {
-    let [town, lat, long] = newArr[0];
-    result[town] = town;
-    result[lat] = lat;
-    result;
-  }
+  console.log(JSON.stringify(result));
 }
-townsToJson([
+townsToJSON([
   '| Town | Latitude | Longitude |',
-  '| Sofia | 42.696552 | 23.32601 |',
+  '| Veliko Turnovo | 42.696552 | 23.32601 |',
   '| Beijing | 39.913818 | 116.363625 |',
 ]);
