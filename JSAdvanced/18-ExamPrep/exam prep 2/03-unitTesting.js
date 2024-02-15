2ab6d5c7321a3679c452c0c24b24237d619f4038
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-15 13:56:41 +0200

    Exam Advanced Exercise 2

--- JSAdvanced/18-ExamPrep/exam prep 2/03-unitTesting.js
+++ JSAdvanced/18-ExamPrep/exam prep 2/03-unitTesting.js
@@ -41,10 +41,53 @@ describe('isGoodLocation', () => {
 
 describe('isLargeEnough', () => {
   it('testing with invalid input', () => {
-    assert.throw(() => findNewApartment.isLargeEnough());
+    assert.throw(() => findNewApartment.isLargeEnough('pesho', 'pesho'));
+    assert.throw(() => findNewApartment.isLargeEnough([], 'pesho'));
+    assert.throw(() =>
+      findNewApartment.isLargeEnough(['pesho', 'gosho'], 'Pesho')
+    );
+    assert.throw(() => findNewApartment.isLargeEnough([], 6));
+    assert.throw(() => findNewApartment.isLargeEnough('Pesho', 5));
+    assert.throw(() => findNewApartment.isLargeEnough([20, 30, 40], '5'));
+    assert.throw(() => findNewApartment.isLargeEnough([20, 30, 40], 'pesho'));
+  });
+  it('testing with valid value', () => {
+    let res = [10, 20, 30];
+    assert.equal(
+      findNewApartment.isLargeEnough([1, 2, 10, 20, 30], 9),
+      res.join(', ')
+    );
+    assert.equal(
+      findNewApartment.isLargeEnough([1, 2, 10, 20, 30], 10),
+      res.join(', ')
+    );
+    assert.equal(findNewApartment.isLargeEnough([1, 2, 10, 20, 30], 50), '');
   });
 });
 
 describe('isItAffordable', () => {
-  it('testing with invalid input', () => {});
+  it('testing with invalid input', () => {
+    assert.throw(() => findNewApartment.describe('50', '60'));
+    assert.throw(() => findNewApartment.describe('pesho', 60));
+    assert.throw(() => findNewApartment.describe(50, 'Gosho'));
+    assert.throw(() => findNewApartment.describe(-50, 20));
+    assert.throw(() => findNewApartment.describe(50, -20));
+    assert.throw(() => findNewApartment.describe(20, 0));
+    assert.throw(() => findNewApartment.describe(0, 20));
+    assert.throw(() => findNewApartment.describe(0, 0));
+  });
+  it('return value is 0', () => {
+    assert.equal(
+      findNewApartment.isItAffordable(10, 9),
+      "You don't have enough money for this house!"
+    );
+    assert.equal(
+      findNewApartment.isItAffordable(10, 11),
+      'You can afford this home!'
+    );
+    assert.equal(
+      findNewApartment.isItAffordable(10, 11),
+      'You can afford this home!'
+    );
+  });
 });
