4180b3471a3ac902c9bedab5cfcc8c291192e891
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-19 16:48:23 +0200

    JS advanced old tasks

---
+++ JSAdvanced/01-syntaxFunctions&StatementsLab/02-stringLength.js
@@ -0,0 +1,6 @@
+function strLength(str1, str2, str3) {
+  const length = str1.length + str2.length + str3.length;
+  console.log(length);
+  console.log(Math.floor(length / 3));
+}
+strLength('chocolate', 'ice cream', 'cake');
