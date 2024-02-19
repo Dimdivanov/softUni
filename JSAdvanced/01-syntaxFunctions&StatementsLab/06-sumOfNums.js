dca7849ad6641fe3617d42cf8700dc5875ba847c
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-19 17:32:55 +0200

    JS advanced old tasks

---
+++ JSAdvanced/01-syntaxFunctions&StatementsLab/06-sumOfNums.js
@@ -0,0 +1,10 @@
+function sumOfNums(n, m) {
+  let num1 = Number(n);
+  let num2 = Number(m);
+  let result = 0;
+  for (let i = num1; i <= num2; i++) {
+    result += i;
+  }
+  console.log(result);
+}
+sumOfNums('1', '5');
