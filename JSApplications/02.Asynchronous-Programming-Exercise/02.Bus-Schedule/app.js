c87518651d1f18666cd6f5f2bd59544a6404fa2a
Dimitar Ivanov <riseabove92@yahoo.com>
2024-02-28 10:56:42 +0200

    Asynchronous Programming Exercise

--- JSApplications/02.Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
+++ JSApplications/02.Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
@@ -1,17 +1,37 @@
 function solve() {
+  let departBtnRef = document.getElementById('depart');
+  let arriveBtnRef = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+  let infoBox = document.querySelector('.info');
+  let url = 'http://localhost:3030/jsonstore/bus/schedule/';
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  const stop = {
+    currentStop: '',
+    nextStop: 'depot',
+  };
+  async function depart() {
+    let response = await fetch(url + stop.nextStop);
+    let data = await response.json();
+    stop.currentStop = data.name;
+    stop.nextStop = data.next;
 
-    return {
-        depart,
-        arrive
-    };
+    infoBox.textContent = `Next stop ${stop.currentStop}`;
+
+    arriveBtnRef.disabled = false;
+    departBtnRef.disabled = true;
+  }
+
+  function arrive() {
+    infoBox.textContent = `Arriving at ${stop.currentStop}`;
+
+    departBtnRef.disabled = false;
+    arriveBtnRef.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
