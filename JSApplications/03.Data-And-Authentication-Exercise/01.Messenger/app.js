e158f1951b41e868cc9b2e7b4a35a5b7148074ec
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-05 16:57:46 +0200

    Data and Authentication - Exercise

--- JSApplications/03.Data-And-Authentication-Exercise/01.Messenger/app.js
+++ JSApplications/03.Data-And-Authentication-Exercise/01.Messenger/app.js
@@ -1,5 +1,39 @@
 function attachEvents() {
-    console.log('TODO...');
+  const btnSendRef = document.getElementById('submit');
+  const btnRefreshRef = document.getElementById('refresh');
+  const textAreaRef = document.getElementById('messages');
+
+  const url = 'http://localhost:3030/jsonstore/messenger';
+
+  btnSendRef.addEventListener('click', onSend);
+  btnRefreshRef.addEventListener('click', onRefresh);
+
+  async function onSend(e) {
+    const authorRef = document.querySelector('input[name="author"]');
+    const messageRef = document.querySelector('input[name="content"]');
+
+    const message = messageRef.value;
+    const author = authorRef.value;
+
+    await fetch(url, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ author: author, content: message }),
+    });
+    messageRef.value = '';
+    authorRef.value = '';
+  }
+  async function onRefresh(e) {
+    textAreaRef.value = '';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    Object.values(data).forEach((msg) => {
+      textAreaRef.value += `${msg.author}: ${msg.content}\n`;
+    });
+    textAreaRef.value = textAreaRef.value.trim();
+  }
 }
 
 attachEvents();
