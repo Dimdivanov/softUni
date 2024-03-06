ec8fee9420d56c3c1b716e7fa2b5b0ae050c9aaa
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-06 20:05:29 +0200

    Data and Authentication - Exercise

--- JSApplications/03.Data-And-Authentication-Exercise/03.Students/app.js
+++ JSApplications/03.Data-And-Authentication-Exercise/03.Students/app.js
@@ -1,29 +1,63 @@
-function students() {
-  const formRef = document.getElementById('form');
+window.addEventListener('load', extractStudents);
 
-  const url = 'http://localhost:3030/jsonstore/collections/students';
+const formRef = document.getElementById('form');
+const firstNameRef = document.querySelector('input[name="firstName"]');
+const lastNameRef = document.querySelector('input[name="lastName"]');
+const facultyNumRef = document.querySelector('input[name="facultyNumber"]');
+const gradeRef = document.querySelector('input[name="grade"]');
 
-  formRef.addEventListener('submit', onSubmit);
-  async function onSubmit(ev) {
-    ev.preventDefault();
-    const formData = new FormData(formRef);
-    const { firstName, lastName, facultyNumber, grade } = Object.fromEntries(
-      formData.entries()
-    );
+const url = 'http://localhost:3030/jsonstore/collections/students';
 
-    await fetch(url, {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ firstName, lastName, facultyNumber, grade }),
-    });
-  }
+formRef.addEventListener('submit', onSubmit);
+async function onSubmit(ev) {
+  ev.preventDefault();
 
-  async function extractStudents(ev) {
-    const response = await fetch(url);
-    const data = await response.json();
-    debugger;
-    return data;
-  }
+  const formData = new FormData(formRef);
+
+  const { firstName, lastName, facultyNumber, grade } = Object.fromEntries(
+    formData.entries()
+  );
+
+  await fetch(url, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ firstName, lastName, facultyNumber, grade }),
+  });
   extractStudents();
 }
-students();
+
+async function extractStudents(ev) {
+  const response = await fetch(url);
+
+  let data = await response.json();
+  Object.values(data).forEach((element) => {
+    if (!element.hasOwnProperty('firstName')) {
+      return;
+    }
+    const modifiedElement = removeLastKeyAndValue(element);
+    tdCreator(modifiedElement);
+  });
+}
+function removeLastKeyAndValue(obj) {
+  const newObj = {};
+  const keys = Object.keys(obj);
+  const lastKey = keys.pop();
+
+  keys.forEach((key) => {
+    newObj[key] = obj[key];
+  });
+
+  return newObj;
+}
+function tdCreator(data) {
+  const tbodyRef = document.querySelector('tbody');
+  const trElement = document.createElement('tr');
+
+  Object.values(data).forEach((el) => {
+    const tdElement = document.createElement('td');
+    tdElement.textContent = el;
+    trElement.appendChild(tdElement);
+  });
+
+  tbodyRef.appendChild(trElement);
+}
