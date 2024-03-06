db967a60aca90b4a2433e6dbd47c4b09b95574fe
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-06 11:40:01 +0200

    Data and Authentication - Exercise

--- JSApplications/03.Data-And-Authentication-Exercise/03.Students/app.js
+++ JSApplications/03.Data-And-Authentication-Exercise/03.Students/app.js
@@ -1,1 +1,29 @@
-console.log('TODO...');
+function students() {
+  const formRef = document.getElementById('form');
+
+  const url = 'http://localhost:3030/jsonstore/collections/students';
+
+  formRef.addEventListener('submit', onSubmit);
+  async function onSubmit(ev) {
+    ev.preventDefault();
+    const formData = new FormData(formRef);
+    const { firstName, lastName, facultyNumber, grade } = Object.fromEntries(
+      formData.entries()
+    );
+
+    await fetch(url, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstName, lastName, facultyNumber, grade }),
+    });
+  }
+
+  async function extractStudents(ev) {
+    const response = await fetch(url);
+    const data = await response.json();
+    debugger;
+    return data;
+  }
+  extractStudents();
+}
+students();
