3fac05404d3a189929faad6cb5dceeba4157640d
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-09 17:20:07 +0200

    Single Page Applications Lab Demo

--- JSApplications/04.JS-Applications-Single-Page-Applications-Lab-Resources/js-apps/demo/src/app.js
+++
@@ -1,8 +0,0 @@
-import { add, product as multiply } from './util.js';
-
-const result = add(5, 3);
-
-console.log(result, multiply(2, 2));
-
-const h1 = document.querySelector('h1');
-h1.textContent += ' Dynamic content.';
