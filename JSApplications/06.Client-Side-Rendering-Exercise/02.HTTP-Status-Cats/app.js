e42f160415056aae1dbf8fd9510d93ea555215e7
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-12 20:38:19 +0200

    client side and rendering lit html exercises

---
+++ JSApplications/06.Client-Side-Rendering-Exercise/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,40 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { cats } from './catSeeder.js';
+
+const root = document.getElementById('allCats');
+
+render(createCatList(cats), root);
+
+function createCatList(cats) {
+  return html`
+    <ul>
+      ${cats.map((cat) => createCatCard(cat))}
+    </ul>
+  `;
+}
+
+function createCatCard(cats) {
+  return html`
+    <li>
+      <img src="./images/${cats.imageLocation}.jpg" width="250" height="250" alt="Card image cap" />
+      <div class="info">
+        <button @click=${toggleCatStatus} class="showBtn">Show status code</button>
+        <div class="status" style="display: none" id="100">
+          <h4>Status Code: ${cats.statusCode}</h4>
+          <p>${cats.statusMessage}</p>
+        </div>
+      </div>
+    </li>
+  `;
+}
+function toggleCatStatus(e) {
+  const container = e.target.parentElement.querySelector('div');
+  const currentState = container.style.display;
+  if (currentState == 'none') {
+    container.style.display = 'block';
+    e.target.textContent = 'Hide status code';
+  } else {
+    container.style.display = 'none';
+    e.target.textContent = 'Show status code';
+  }
+}
