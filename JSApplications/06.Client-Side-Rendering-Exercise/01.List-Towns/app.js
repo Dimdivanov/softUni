09a97bdf006f535011fa6a3965747c02c92fde1f
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-12 17:00:12 +0200

    client side and rendering lit html exercises

--- JSApplications/06.Client-Side-Rendering-Exercise/01.List-Towns/app.js
+++ JSApplications/06.Client-Side-Rendering-Exercise/01.List-Towns/app.js
@@ -1,6 +1,22 @@
-import { html, render } from 'lit-html';
+import { html, render } from './node_modules/lit-html/lit-html.js';
 
-const inputIdRef = document.getElementById('towns');
-const btnIdRef = document.getElementById('btnLoadTowns');
+const form = document.querySelector('form').addEventListener('submit', onsubmit);
+const root = document.getElementById('root');
+function onsubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const townList = formData.get('towns').split(', ');
+  renderer(createTemplate(townList));
+}
 
-const displayContent = (towns) => {};
+function createTemplate(towns) {
+  return html`
+    <ul>
+      ${towns.map((town) => html`<li>${town}</li>`)}
+    </ul>
+  `;
+}
+
+function renderer(temp) {
+  render(temp, root);
+}
