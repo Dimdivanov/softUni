0b7dbcc4d4dc4d347ce42dd756e7866c509ef9ba
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-13 12:09:43 +0200

    client side and rendering lit html exercises

--- JSApplications/06.Client-Side-Rendering-Exercise/03.Search-in-List/search.js
+++ JSApplications/06.Client-Side-Rendering-Exercise/03.Search-in-List/search.js
@@ -1,3 +1,35 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+const button = document.querySelector('button').addEventListener('click', search);
+const townsRoot = document.getElementById('towns');
+const resultRoot = document.getElementById('result');
+const inputRef = document.getElementById('searchText');
+
+update();
+function update(match) {
+  render(ulTemplate(towns, match), townsRoot);
+}
+
+function ulTemplate(towns, match) {
+  return html`
+    <ul>
+      ${towns.map((town) => createLiTemplate(town, match?.includes(town)))}
+    </ul>
+  `;
+}
+
+function createLiTemplate(town, isActive) {
+  return html`<li class=${isActive ? 'active' : ''}>${town}</li>`;
+}
+
 function search() {
-   // TODO
+  const searchText = inputRef.value;
+  const match = towns.filter((town) => town.includes(searchText));
+  update(match);
+  renderMatchCount(match.length);
+}
+
+function renderMatchCount(count) {
+  render(html`${count} matches found`, resultRoot);
 }
