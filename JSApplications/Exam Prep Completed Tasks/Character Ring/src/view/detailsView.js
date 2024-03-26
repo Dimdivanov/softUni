eaedf60c2c0a7eca25a08c2006fbceb55848ce75
Dimitar Ivanov <riseabove92@yahoo.com>
2024-03-26 20:23:10 +0200

    Character Ring 115/115 - like implemented

--- JSApplications/Exam Prep Completed Tasks/Character Ring/src/view/detailsView.js
+++ JSApplications/Exam Prep Completed Tasks/Character Ring/src/view/detailsView.js
@@ -2,7 +2,7 @@ import { html } from '../../node_modules/lit-html/lit-html.js';
 import { dataService } from '../service/dataService.js';
 import { userHelper } from '../utility/userHelper.js';
 
-const detailsTemp = (char, isOwner, isLoggedIn) => html`
+const detailsTemp = (char, isOwner, isLoggedIn, likedTimes, userLiked, onClick) => html`
   <section id="details">
     <div id="details-wrapper">
       <img id="details-img" src=${char.imageUrl} alt="example1" />
@@ -14,15 +14,15 @@ const detailsTemp = (char, isOwner, isLoggedIn) => html`
             <p id="more-info">${char.moreInfo}</p>
           </div>
         </div>
-        <h3>Is This Useful:<span id="likes">0</span></h3>
+        <h3>Is This Useful:<span id="likes">${likedTimes}</span></h3>
         <!--Edit and Delete are only for creator-->
         <div id="action-buttons">
           ${isOwner ? buttonsDisplay(char._id) : ''}
-          ${isOwner || !isLoggedIn
+          ${isOwner || !isLoggedIn || userLiked
             ? ''
             : html`
                 <!--Bonus - Only for logged-in users ( not authors )-->
-                <a href="javascript:void(0)" id="like-btn">Like</a>
+                <a @click="${onClick}" href="javascript:void(0)" id="like-btn">Like</a>
               `}
         </div>
       </div>
@@ -41,8 +41,15 @@ export async function showDetails(ctx) {
   const charId = ctx.params.id;
   const char = await dataService.details(charId);
   const isOwner = userHelper.hasOwner(char._ownerId);
-  const isLoggedIn = userHelper.getUserId();
-  
-  
-  ctx.render(detailsTemp(char, isOwner, isLoggedIn));
+  const userId = userHelper.getUserId();
+  const isLoggedIn = !!userId;
+
+  let likedTimes = await dataService.getTotalLikes(charId);
+  let userLiked = await dataService.didUserLiked(charId, userId);
+  //new here
+  async function onClick() {
+    await dataService.like(charId);
+    showDetails(ctx);
+  }
+  ctx.render(detailsTemp(char, isOwner, isLoggedIn, likedTimes, userLiked, onClick));
 }
