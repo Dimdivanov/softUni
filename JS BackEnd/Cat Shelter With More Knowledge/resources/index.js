2948784f34a77080cfcbc907727a37e1dc1578d9
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-17 12:03:41 +0300

    catShelter remastered progress 1%

--- JS BackEnd/Cat Shelter With More Knowledge/resources/index.js
+++ JS BackEnd/Cat Shelter With More Knowledge/resources/index.js
@@ -5,12 +5,6 @@ const fs = require('fs');
 const indexHtmlTemplate = require('./views/home/index');
 http
   .createServer((req, res) => {
-    fs.readFile('./content/images/pawprint.ico', (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(data);
-    });
     res.writeHead(200, {
       'Content-Type': 'text/html',
     });
