8ba6b170f0ea094944131725f1fb785e7cfe2e84
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-22 16:16:56 +0300

    updated folder names and example of Express

---
+++ JS BackEnd/ExpressJS and Templating/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(3000, () => console.log('Server is up on port 3000...'));
