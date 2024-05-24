35782a435037dac4395589fcae9ca5c29123a34b
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-24 16:00:20 +0300

    update

--- JS BackEnd/ExpressJS and Templating/index.js
+++ JS BackEnd/ExpressJS and Templating/index.js
@@ -1,7 +1,42 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const handlebars = require('express-handlebars');
+
+//add handlebars to express
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+//using static
+app.use(express.static('public')); //load all kinds of files from that directory
+
+//Add third-party middleware
+const bodyParser = express.urlencoded({ extended: false }); //this is a func
+app.use(bodyParser); //req.body attaches the parsed post
+
+//Add Middleware
+//=global middleware
+app.use((req, res, next) => {
+  console.log(`Global middleware on ${req.method}: ${req.path}`);
+  next();
+});
+//=partial route middleware
+app.use('/cats', (req, res, next) => {
+  console.log('middleware on cats');
+  next();
+});
+//=specific route middleware
+const someSpecificFunc = (req, res, next) => {
+  console.log('specific middleware');
+  next();
+};
+app.get('/specificpath', someSpecificFunc, (req, res) => {
+  res.send('some specific route with middleware');
+});
+
+//Add Routes
 //get, post, put, delete, update etc.
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
@@ -46,5 +81,5 @@ app.get('old-path-bla', (req, res) => {
 });
 
 //modular routers
-//make hierarchy
+//make hierarchy routers
 app.listen(3000, () => console.log('Server is up on port 3000...'));
