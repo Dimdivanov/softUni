c4a4a8c8ce2cf942a54d83288d504f6aded9ea40
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-25 13:03:43 +0300

    catShelter 3.0 with express & handlebars 10%

--- JS BackEnd/CatShelter with Express/index.js
+++ JS BackEnd/CatShelter with Express/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 //express-handlebars
-app.engine('hbs', handlebars.engine({ extname: 'hbs', defaultLayout: 'main' }));
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 //loading css and other files
@@ -19,6 +19,12 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/cats/add-breed', (req, res) => {
+  res.render('addBreed');
+});
+app.get('/cats/add-cat', (req, res) => {
+    res.render('addCat');
+  });
 app.listen(port, () => {
   console.log('server is up on 30000');
 });
