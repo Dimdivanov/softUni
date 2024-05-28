53ae32ef55f93b294bdda0f0415246abc8426370
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-28 12:06:53 +0300

    routes added , templates work

--- JS BackEnd/Movie Magic part 1/src/index.js
+++ JS BackEnd/Movie Magic part 1/src/index.js
@@ -10,12 +10,21 @@ app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
-//Express Config
+//Express Config -- path had to be changed
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 //Routes
 app.get('/', (req, res) => {
   res.render('home');
+});
+app.get('/create', (req, res) => {
+  res.render('create');
+});
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 app.listen(PORT, console.log(`App is running on ${PORT}...`));
