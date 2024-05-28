f893f92b65e8749882956bd91657db622a186462
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-28 11:49:31 +0300

    add handlebars to project

--- JS BackEnd/Movie Magic part 1/src/index.js
+++ JS BackEnd/Movie Magic part 1/src/index.js
@@ -5,13 +5,16 @@ const app = express();
 const PORT = 3000;
 
 //Handlebars config
-app.engine('hbs', handlebars.engine({ extname: 'hbs'}))
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+//Loading css and other files
+app.use(express.static('public'));
 
 //Routes
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.render('home');
 });
-
 
 app.listen(PORT, console.log(`App is running on ${PORT}...`));
