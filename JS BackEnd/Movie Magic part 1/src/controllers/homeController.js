01e3cbfad938dd0b81c7b1c28266d9970f8a886a
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-29 15:04:54 +0300

    page 404 done

--- JS BackEnd/Movie Magic part 1/src/controllers/homeController.js
+++ JS BackEnd/Movie Magic part 1/src/controllers/homeController.js
@@ -8,6 +8,9 @@ router.get('/', (req, res) => {
 router.get('/about', (req, res) => {
   res.render('about');
 });
+router.get('/404', (req, res)=>{
+  res.render('404');
+});
 //move these too
 router.get('/search', (req, res) => {
   res.render('search');
--- JS BackEnd/Movie Magic part 1/src/index.js
+++ JS BackEnd/Movie Magic part 1/src/index.js
@@ -16,5 +16,9 @@ handlebarsConfigurator(app);
 app.use(homeController);
 app.use('/movies', movieController);
 app.use('/movies/:movieId', movieController);
+//all routes that aren't existing redirect to 404
+app.get('*', (req, res) => {
+  res.redirect('/404');
+});
 
 app.listen(PORT, console.log(`App is running on ${PORT}...`));
