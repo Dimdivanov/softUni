adb11406233656dcf7c3ba96d407f81fe4bf96a8
Dimitar Ivanov <riseabove92@yahoo.com>
2024-05-31 18:13:19 +0300

    mongoose virtual example

--- JS BackEnd/MongoDB Lab Examples/index.js
+++ JS BackEnd/MongoDB Lab Examples/index.js
@@ -5,7 +5,8 @@ async function connectDb() {
   await mongoose.connect('mongodb://127.0.0.1:27017/db');
   console.log('Db connected successfully');
   const cats = await Cat.find();
-  console.log(cats);
+  // cats.forEach((cat) => cat.greet());
+  cats.forEach((cat) => console.log(cat.infoCat));
 }
 
 connectDb();
--- JS BackEnd/MongoDB Lab Examples/models/Cat.js
+++ JS BackEnd/MongoDB Lab Examples/models/Cat.js
@@ -5,7 +5,16 @@ const catSchema = new mongoose.Schema({
   age: Number,
   breed: String,
 });
+//can attach methods to the schema
+catSchema.methods.greet = function () {
+  console.log(`Hello I\'m ${this.name}`);
+};
+//creating a getter / setter which will not be displayed in the db
+catSchema.virtual('infoCat').get(function () {
+  return `My name is ${this.name} and I'm ${this.breed}`;
+});
 
+//create model
 const Cat = mongoose.model('Cat', catSchema);
 
 module.exports = Cat;
