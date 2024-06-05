aef87f039bb5e427c1486badec9d155d76ef723d
Dimitar Ivanov <riseabove92@yahoo.com>
2024-06-05 19:00:39 +0300

    added post action

--- JS BackEnd/Movie Magic part 2/src/controllers/castController.js
+++ JS BackEnd/Movie Magic part 2/src/controllers/castController.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
-router.get('/create', (req, res)=>{
-    res.render('cast/create');
+router.get('/create', (req, res) => {
+  res.render('cast/create');
+});
+router.post('/create', (req, res) => {
+  const body = req.body;
+  
+  res.redirect('/')
 });
 
 module.exports = router;
