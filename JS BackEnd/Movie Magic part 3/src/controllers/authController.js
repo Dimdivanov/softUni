cd9cf3be51b39f5d2d6853accac1cb7d5f03c10b
Dimitar Ivanov <riseabove92@yahoo.com>
2024-06-12 09:33:40 +0300

    authManager created

--- JS BackEnd/Movie Magic part 3/src/controllers/authController.js
+++ JS BackEnd/Movie Magic part 3/src/controllers/authController.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const authManager = require('../managers/authManager');
 
 router.get('/register', (req, res) => {
-  console.log('in register');
   res.render('auth/register');
+});
+router.post('/register', async (req, res) => {
+  const userData = req.body;
+  await authManager.register(userData);
+  res.redirect('auth/login');
 });
 
 module.exports = router;
