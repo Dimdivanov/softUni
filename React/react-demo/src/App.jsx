5e3d2a000ed750e31eeb61f15691e7dbf24f07d8
Dimitar Ivanov <riseabove92@yahoo.com>
2024-06-30 10:51:42 +0300

    creating server userController and Routes

--- React/react-demo/src/App.jsx
+++ React/react-demo/src/App.jsx
@@ -8,23 +8,7 @@ function App() {
       <Navbar />
       <Banner />
       <About />
-      {/*-----Video Start-----*/}
-      <section className="video-section prelative text-center white">
-        <div className="section-padding video-overlay">
-          <div className="container">
-            <h3>Watch Now</h3>
-            <i className="fa fa-play" id="video-icon" aria-hidden="true" />
-            <div className="video-popup">
-              <div className="video-src">
-                <div className="iframe-src">
-                  <iframe src="https://www.youtube.com/embed/Ku52zNnft8k?rel=0&showinfo=0" allowFullScreen="" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/*-----Video End-----*/}
+      <Video />
       {/*-----Features Start-----*/}
       <section className="feature section-padding" data-scroll-index={2}>
         <div className="container">
