57966ccdbd8328f725975d5a4d0609110ba6967a
Dimitar Ivanov <riseabove92@yahoo.com>
2024-07-30 11:46:11 +0300

    latest games displayed correctly

--- React/react-routing-demo/client/src/components/home/Home.jsx
+++ React/react-routing-demo/client/src/components/home/Home.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+import gamesAPI from '../../api/games-api';
+import LatestGames from './latest-game/LatestGame';
+
 export default function Home() {
+    const [latestGames, setLatestGames] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            //to do modify to get last 3 games
+            const result = await gamesAPI.getAll();
+
+            setLatestGames(result);
+        })();
+    }, []);
     return (
         <>
             <section id="welcome-world">
@@ -10,63 +24,13 @@ export default function Home() {
 
                 <div id="home-page">
                     <h1>Latest Games</h1>
-
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/CoverFire.png" />
-                        </div>
-                        <h3>Cover Fire</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/ZombieLang.png" />
-                        </div>
-                        <h3>Zombie Lang</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/MineCraft.png" />
-                        </div>
-                        <h3>MineCraft</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-
-                    <p className="no-articles">No games yet</p>
+                    {latestGames.length > 0 ? (
+                        latestGames.map((game) => (
+                            <LatestGames key={game._id} {...game} />
+                        ))
+                    ) : (
+                        <p className="no-articles">No games yet</p>
+                    )}
                 </div>
             </section>
         </>
---
+++ React/react-routing-demo/client/src/components/home/latest-game/LatestGame.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+export default function LatestGames({ imageUrl, title, _id }) {
+    return (
+        <>
+            <div className="game">
+                <div className="image-wrap">
+                    <img src={imageUrl} />
+                </div>
+                <h3>{title}</h3>
+                <div className="rating">
+                    <span>☆</span>
+                    <span>☆</span>
+                    <span>☆</span>
+                    <span>☆</span>
+                    <span>☆</span>
+                </div>
+                <div className="data-buttons">
+                    <Link to={`/games/${_id}/details`} className="btn details-btn">
+                        Details
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
+}
