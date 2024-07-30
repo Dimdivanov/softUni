ba834a1fa895cdfe0c0c240d2cec2360a8ef4659
Dimitar Ivanov <riseabove92@yahoo.com>
2024-07-30 10:42:43 +0300

    server data added, games-api updated

--- React/react-routing-demo/client/src/api/games-api.js
+++ React/react-routing-demo/client/src/api/games-api.js
@@ -3,5 +3,7 @@ import * as request from '../api/requester';
 const BASE_URL = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
-    return await request.get(BASE_URL);
+    const result = await request.get(BASE_URL);
+    const games = Object.values(result);
+    return games;
 };
