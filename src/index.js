0b75450c29d410450446f6c13b000ec2cf6e6d73
Dimitar Ivanov <riseabove92@yahoo.com>
2024-09-11 20:29:02 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as yup from 'yup';
+
+export const basicSchema = yup.object().shape({
+    email: yup.string().email('Please enter a valid email!').required('Required'),
+    password: yup.string().required('Required'),
+});
