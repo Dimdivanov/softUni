edbe2c854afd2dd5d6e09f5e4dc02003a9851233
Dimitar Ivanov <riseabove92@yahoo.com>
2024-09-11 19:44:29 +0300

    react-formik-demo

--- React/react-form-demo-Formik/src/schemas/index.js
+++ React/react-form-demo-Formik/src/schemas/index.js
@@ -4,3 +4,4 @@ export const basicSchema = yup.object().shape({
     email: yup.string().email('Please enter a valid email!').required('Required'),
     password: yup.string().required('Required'),
 });
+//testing
