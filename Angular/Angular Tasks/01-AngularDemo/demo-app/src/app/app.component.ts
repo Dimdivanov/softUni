4e9f7c554df1dd458584947146f01900fb277340
Dimitar Ivanov <riseabove92@yahoo.com>
2024-11-09 15:30:33 +0200

    angular first component created

--- Angular/Angular Tasks/01-AngularDemo/demo-app/src/app/app.component.ts
+++ Angular/Angular Tasks/01-AngularDemo/demo-app/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './app-navigation-component/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'demo-app';
+  title = 'first-demo-app';
 }
