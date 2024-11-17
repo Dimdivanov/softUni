0ebd567f33dc8c97618ed5915d987df35ebae314
Dimitar Ivanov <riseabove92@yahoo.com>
2024-11-17 15:18:49 +0200

    post blog workshop, task started

---
+++ Angular/Angular Tasks/02-Component-Workshop/post-blog/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { MainComponent } from './main/main.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'post-blog';
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get(
+        'https://angular-component-workshop-default-rtdb.europe-west1.firebasedatabase.app/.json'
+      )
+      .subscribe((x) => console.log(x));
+  }
+}
